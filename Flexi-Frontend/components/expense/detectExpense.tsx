1a7527721dd728d1f8694af6d548e5fbf3f2ce9b
Atomic649 <atomic.intergroup@gmail.com>
2025-05-05 02:39:29 +0700

    PDF password handle frontend

--- Flexi-Frontend/components/expense/detectExpense.tsx
+++ Flexi-Frontend/components/expense/detectExpense.tsx
@@ -10,6 +10,7 @@ import {
   Alert,
   Dimensions,
   Platform, // Import Platform for platform checks
+  TextInput, // Import TextInput for password input
 } from "react-native";
 
 import * as DocumentPicker from "expo-document-picker";
@@ -40,6 +41,9 @@ export default function DetectExpense() {
   const [isCreateExpenseVisible, setIsCreateExpenseVisible] = useState(false);
   const [isCreateSuccess, setIsCreateSuccess] = useState(false);
   const [refreshing, setRefreshing] = useState(false);
+  const [password, setPassword] = useState<string>(""); // State for password
+  const [passwordModalVisible, setPasswordModalVisible] =
+    useState<boolean>(false); // State for password modal
 
   // auto delete if save is false
   const autoDelete = async () => {
@@ -81,7 +85,58 @@ export default function DetectExpense() {
       }
     }
   };
+  const confirmAndProcessPdfwithPassword = async () => {
+    setModalVisible(false);
+    setLoading(true);
+    onRefresh(); // refreshing the table
+    try {
+      const memberId = await getMemberId();
+      const filePath = pdfUri;
+      console.log("🔥filePath", filePath);
 
+      if (memberId && filePath) {
+        const formData = new FormData();
+
+        if (Platform.OS === "web") {
+          // Handle file upload for web
+          const response = await fetch(filePath);
+          const blob = await response.blob();
+          formData.append("filePath", blob, "file.pdf");
+        } else {
+          // Handle file upload for native platforms
+          formData.append("filePath", {
+            uri: filePath,
+            name: "file.pdf",
+            type: "application/pdf",           
+          } as unknown as Blob);
+        }
+
+        formData.append("memberId", memberId);
+
+        const response = await CallAPIExpense.extractPDFExpenseAPI(formData);
+        if (response.message === "Expenses created successfully") {
+          setError(null);
+          setExpenses(response.expenses);
+          console.log("🔥response", response);
+        } else {
+          console.error("No expenses found in the PDF.");
+        }
+      } else {
+        console.error("Member ID is null or filePath is null");
+      }
+    } catch (error: any) {
+      console.error("Error fetching expenses:", error);
+      if (error.message === "Duplicate data found") {
+        setError("Duplicate data found");
+      } else if (error.message === "No password given") {
+        setPasswordModalVisible(true);
+      } else {
+        setError("Failed to process PDF");
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
   const confirmAndProcessPdf = async () => {
     setModalVisible(false);
     setLoading(true);
@@ -125,12 +180,24 @@ export default function DetectExpense() {
       console.error("Error fetching expenses:", error);
       if (error.message === "Duplicate data found") {
         setError("Duplicate data found");
+      } else if (error.message === "No password given") {
+        setPasswordModalVisible(true);
       } else {
         setError("Failed to process PDF");
       }
     } finally {
       setLoading(false);
     }
+  };
+
+
+  const handlePasswordSubmit = () => {
+    if (password.trim() === "") {
+      Alert.alert(t("common.error"), t("expense.alerts.emptyPassword"));
+      return;
+    }
+    setPasswordModalVisible(false);
+    confirmAndProcessPdf(); // Proceed with PDF processing
   };
 
   // refreshing table
@@ -199,11 +266,15 @@ export default function DetectExpense() {
   };
 
   return (
-    <SafeAreaView className={`h-full ${useBackgroundColorClass()} items-center`}>
-      <View className="flex-row items-center justify-between  py-1"
-      style={{
-        width : Platform.OS === "web" ? "50%" : "100%",
-      }}>
+    <SafeAreaView
+      className={`h-full ${useBackgroundColorClass()} items-center`}
+    >
+      <View
+        className="flex-row items-center justify-between  py-1"
+        style={{
+          width: Platform.OS === "web" ? "50%" : "100%",
+        }}
+      >
         <TouchableOpacity
           className="items-center justify-center"
           style={{
@@ -273,9 +344,13 @@ export default function DetectExpense() {
         <View className="flex-1 justify-center items-center ">
           <View
             className="p-8 rounded-2xl"
-            style={{ backgroundColor: theme === "dark" ? "#282625" : "#edeceb" }}
+            style={{
+              backgroundColor: theme === "dark" ? "#282625" : "#edeceb",
+            }}
           >
-            <CustomText className="text-center ">{t("expense.table.pdferror")}</CustomText>
+            <CustomText className="text-center ">
+              {t("expense.table.pdferror")}
+            </CustomText>
           </View>
         </View>
       )}
@@ -305,8 +380,8 @@ export default function DetectExpense() {
             style={{ width: "90%", height: "62%" }}
           >
             {loading && <ActivityIndicator size="large" />}
-            {pdfUri && (
-              Platform.OS !== "web" ? (
+            {pdfUri &&
+              (Platform.OS !== "web" ? (
                 <WebView
                   className="flex-1 w-full h-full"
                   originWhitelist={["*"]}
@@ -324,8 +399,7 @@ export default function DetectExpense() {
                     title="PDF Preview"
                   />
                 </View>
-              )
-            )}
+              ))}
             <View className="flex-row justify-between px-10 mt-4">
               <Button
                 title={t("common.cancel")}
@@ -334,7 +408,69 @@ export default function DetectExpense() {
               />
               <Button
                 title={t("common.confirm")}
-                onPress={confirmAndProcessPdf}
+                onPress={() => {
+                  confirmAndProcessPdfwithPassword();       
+                                             }}
+                color="#ff1713"
+              />
+            </View>
+          </View>
+        </View>
+      </Modal>
+
+      <Modal
+        visible={passwordModalVisible}
+        transparent={true}
+        animationType="slide"
+        onRequestClose={() => setPasswordModalVisible(false)}
+      >
+        <View
+          className="flex-1 justify-center items-center"
+          style={{
+            backgroundColor: theme === "dark" ? "#000000aa" : "#efefefaa",
+          }}
+        >
+          <View
+            className=" p-6 rounded-lg"
+            style={{
+              width: "90%",
+              maxHeight: "30%",
+              backgroundColor: theme === "dark" ? "#2D2D2D" : "#ffffff",
+            }}
+          >
+            <CustomText className="text-center text-lg font-pmedium mb-4">
+              {t("expense.alerts.enterPassword")}
+            </CustomText>
+            {/* <Text className="text-center text-base font-normal mb-2 ">
+              {t("expense.alerts.deleteFile")}
+            </Text> */}
+            <TextInput
+              style={{
+                borderWidth: 1,
+                borderColor: theme === "dark" ? "#6d6c67" : "#adaaa6",
+                borderRadius: 8,
+                padding: 10,
+                marginBottom: 20,
+                color: theme === "dark" ? "#ffffff" : "#000000",
+              }}
+              placeholder={t("expense.alerts.password")}
+              placeholderTextColor={theme === "dark" ? "#6d6c67" : "#adaaa6"}
+              secureTextEntry={true}
+              value={password}
+              onChangeText={setPassword}
+            />
+            <View className="flex-row justify-between">
+              <Button
+                title={t("common.cancel")}
+                onPress={() => setPasswordModalVisible(false)}
+                color="#0d70f0"                
+              />
+              <Button
+                title={t("common.confirm")}
+                onPress={() => {
+                  handlePasswordSubmit();
+                  console.log("Password", password);
+                }}                
                 color="#ff1713"
               />
             </View>
