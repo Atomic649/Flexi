e54130cb2c595242d6775450ac5eb86da6e24fcd
Atomic649 <atomic.intergroup@gmail.com>
2025-05-23 16:03:53 +0700

    Login and register on main top bar on web

--- Flexi-Frontend/components/MainTopBar.tsx
+++ Flexi-Frontend/components/MainTopBar.tsx
@@ -3,13 +3,21 @@ import { View, TouchableOpacity, Image, Text, Platform } from "react-native";
 import { router } from "expo-router";
 import { CustomText } from "@/components/CustomText";
 import { icons, images } from "@/constants";
+import i18n from "@/i18n";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
+// ฟังก์ชันสลับภาษา
+const toggleLanguage = () => {
+  const newLang = i18n.language === "th" ? "en" : "th";
+  AsyncStorage.setItem("language", newLang);
+  i18n.changeLanguage(newLang);
+};
 
 const mainTopBar = (
   theme: string,
   registeredUsers: number | null,
   businessAvatar: string | null,
-  businessName: string | null,  
+  businessName: string | null
 ) => ({
   headerShown: true,
   headerStyle: {
@@ -43,7 +51,69 @@ const mainTopBar = (
       className="flex-row items-center"
       style={{ paddingRight: Platform.OS === "web" ? "0.5%" : 0 }}
     >
-      <TouchableOpacity onPress={() => router.push("/roadmap")} className="mr-5">
+      {/* if web (Dimension of screen >786) show log in and register button and language setting */}
+      <TouchableOpacity
+        onPress={() => router.push("/login")}
+        className="mr-5"
+        style={{
+          display: Platform.OS === "web" ? "flex" : "none",
+        }}
+      >
+        <CustomText
+          weight="semibold"
+          style={{
+            color: theme === "dark" ? "#a1a1a1" : "#4e4b47",
+          }}
+          className={`text-base font-bold `}
+        >
+          Log In
+        </CustomText>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => router.push("/register")}
+        className="mr-5"
+        style={{
+          display: Platform.OS === "web" ? "flex" : "none",
+        }}
+      >
+        <CustomText
+          weight="semibold"
+          style={{
+            color: theme === "dark" ? "#18181b" : "#ffffff",
+            backgroundColor: theme === "dark" ? "#07e5c0" : "#07e5c0",
+            paddingVertical: 8,
+            paddingHorizontal: 16,
+            borderRadius: 20,
+          }}
+          className={`text-base font-bold `}
+        >
+          Register
+        </CustomText>
+      </TouchableOpacity>
+      {/* ปุ่มเปลี่ยนภาษา */}
+      <TouchableOpacity
+        style={{
+          display: Platform.OS === "web" ? "flex" : "none",
+        }}
+        onPress={toggleLanguage}
+        className="  rounded-full"
+      >
+        <View className="flex-row items-center gap-2 !bg-transparent px-2">
+          <Image
+            source={i18n.language === "th" ? icons.flagen : icons.flagth}
+            style={{ width: 20, height: 20 }}
+            resizeMode="contain"
+          />
+          <CustomText className="!text-white text-sm">
+            {i18n.language === "th" ? "EN" : "ไทย"}
+          </CustomText>
+        </View>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={() => router.push("/roadmap")}
+        className="mr-5"
+      >
         <Image
           source={icons.businessman}
           resizeMode="stretch"
