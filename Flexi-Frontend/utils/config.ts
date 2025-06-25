1d1d3c1e9987834ec6411b3beb5c8b80f119f668
Atomic649 <atomic.intergroup@gmail.com>
2025-06-25 13:52:54 +0700

    config fix

--- Flexi-Frontend/utils/config.ts
+++ Flexi-Frontend/utils/config.ts
@@ -1,14 +1,13 @@
 //-------------Development Mode------------------
-export const API_URL = 'http://192.168.1.103:3000/'; //Nodemon
+//export const API_URL = 'http://192.168.1.103:3000/'; //Nodemon
 //export const API_URL = 'https://192.168.1.103:3000/'; //Docker
 
 //imageURL
-export const MOCKUP_IMAGE_URL = 'http://192.168.1.103:3000/uploads/mock/';
+//export const MOCKUP_IMAGE_URL = 'http://192.168.1.103:3000/uploads/mock/';
 
 
 
  //-------------Production Mode------------------
-//export const API_URL = 'https://api.flexibusinesshub.com/'; // Replace with your backend URL
- //imageURL
- //export const IMAGE_URL = 'https://ads.flexibusinesshub.com/uploads/images/';
-
+export const API_URL = 'https://api.flexibusinesshub.com/'; // Replace with your backend URL
+export const IMAGE_URL = 'https://api.flexibusinesshub.com/uploads/images/';
+export const MOCKUP_IMAGE_URL = 'https://api.flexibusinesshub.com/uploads/mock/';
